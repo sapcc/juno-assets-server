999165aece43a17770af76ac0c4b2ca3f3b05a28
d064310 <andreas.pfau@sap.com>
2024-03-15 05:16:07 +0100

    importmap generator: fix syntax error

--- scripts/esm_build/generate_importmap.mjs
+++ scripts/esm_build/generate_importmap.mjs
@@ -101,7 +101,7 @@ console.log("===SERVER_ROOT===", SERVER_ROOT)
 // generate importmap entry url
 const importmapEntryUrl = (path, settings = {}) => {
   let entryUrl = new URL(pathLib.relative(SERVER_ROOT, path), options.baseUrl)
-  if (settings.timestamp) url.searchParams.set("t", TIMESTAMP)
+  if (settings.timestamp) entryUrl.searchParams.set("t", TIMESTAMP)
 
   return entryUrl
 }
